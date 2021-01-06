415477f19c8edd656505bb9d9b340550731048d2
Qianze Zhang <qianze.zhang@hotmail.com>
2021-01-06 17:30:42 +0800

    Modify the apk of the sound network and integrate the XSwitch interface at the same time.
    Modify the apk of the sound network and integrate the XSwitch interface at the same time.

---
+++ One-to-One-Voice/Agora-Android-Voice-Tutorial-1to1/app/src/main/java/io/agora/tutorials1v1acall/CommonConstant.java
@@ -0,0 +1,32 @@
+package io.agora.tutorials1v1acall;
+
+public class CommonConstant {
+    public static final String PREFERENCE_NAME = "MyPrefs";
+
+    // APP ID
+    public static final String APP_ID = "app_id";
+
+    // Token extra
+    public static final String TOKEN = "token";
+
+    // Channel extra
+    public static final String CHANNEL = "channel";
+
+    public static final String USER_ID = "user_id";
+
+    public static final String X_SWITCH_LOGIN_NAME = "x_switch_login_name";
+
+    public static final String X_SWITCH_PASSWORD = "x_switch_password";
+
+    public static final String X_SWITCH_DOMAIN = "x_switch_domain";
+
+    public static final String X_SWITCH_TOKEN = "x_switch_token";
+
+    public static final String X_SWITCH_SIP_NUMBER = "x_switch_sip_number";
+
+    public static final String GET_TOKEN_URL = "https://cloud1.xswitch.cn/api/cloud/token";
+
+    public static final String CALL_PSTN_URL = "https://cloud1.xswitch.cn/api/cloud/callPSTN";
+
+    public static final String CALL_HANG_UP_URL = "https://cloud.xswitch.cn/api/cloud/calls/";
+}
